e1ac7a8b3e70cb0e090511b62d6189f7a4fea675
Filippo Montani <montani@orbitalwaves.it>
2023-12-03 20:42:27 +0100

    d3 shorter and uglier

--- day3/main.py
+++ day3/main.py
@@ -1,33 +1,23 @@
 import re
 
 with open("input.txt") as f:
-    a = f.read().splitlines()    
+    a = f.read().splitlines()
+
+def solver(regex1, regex2, callback):
+    tot = 0
+    for y, line in enumerate(a):
+        matches = re.finditer(regex1, line)
+        for match in matches:
+            results = []
+            ran = [y-1 if y>0 else 0, (y+1 if y<len(a)-1 else y)+1]
+            for  j in range(*ran):
+                results+=[i.group() for i in re.finditer(regex2, a[j]) if i.start() <= match.end() and i.end() >= match.start()]
+            tot+=callback(results, match)
+    print(tot)
 #prob 1
-tot = 0
-
-for y, line in enumerate(a):
-    matches = re.finditer(r"\d+", line)
-    for match in matches:
-        syms = []
-        ran = [y-1 if y>0 else 0, (y+1 if y<len(a)-1 else y)+1]
-        for  j in range(*ran):
-            syms+=[i.group() for i in re.finditer(r'[^.\d]', a[j]) if i.start() <= match.end() and i.start() >= match.start()-1]
-        if len(syms):
-            tot+=int(match.group())
-print(tot)
-
+solver(r"\d+", r"[^.\d]", lambda results, match: int(match.group()) if len(results) else 0)
 #prob 2
-tot=0
-for y, line in enumerate(a):
-    multips = [m.start() for m in re.finditer('\*', line)]
-    for m in multips:
-        nums = []
-        ran = [y-1 if y>0 else 0, (y+1 if y<len(a)-1 else y)+1]
-        for  j in range(*ran):
-            nums+=[int(i.group()) for i in re.finditer(r'\d+', a[j]) if i.start() <= m+1 and i.end()-1 >= m-1]
-        if len(nums) == 2:
-            tot+= nums[0]*nums[1]
-print(tot)
+solver(r"\*", r"\d+", lambda results, match: int(results[0])*int(results[1]) if len(results) == 2  else 0)
 
 
 
